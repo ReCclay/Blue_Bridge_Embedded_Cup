8aeaa9ab7ff7731a6414d6447d40189b70ffeef6
CLAY <317400877@qq.com>
2019-01-24 16:19:51 +0800

    删除冗余的lcd，加入定时器中断

---
+++ LED_Timer/Source/led.c
@@ -0,0 +1,22 @@
+#include "stm32f10x.h"
+
+void LEDInit(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC|RCC_APB2Periph_GPIOD, ENABLE);//设能PC和PD口
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);//设置573锁存器对应的使能端PD2
+	
+	GPIO_InitStructure.GPIO_Pin = 0xFF00;//注意看这里
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);//对应PC8-PC15
+	
+	GPIOD->ODR |= (1<<2);//PD2置1，使能573锁存器
+	GPIOC->ODR |= 0xFF00;//PC8-PC15置1，初始状态熄灭小灯
+	GPIOD->ODR &= ~(1<<2);//PD2清0，关闭573锁存器
+}
+
---
+++ LED_Timer/Source/led.h
@@ -0,0 +1,8 @@
+#ifndef _LED_H
+#define _LED_H
+
+
+void LEDInit(void);
+
+#endif
+
