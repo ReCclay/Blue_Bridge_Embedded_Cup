93b05e539a41d580b5e8b53bf8ad89dbde0c0fc0
CLAY <317400877@qq.com>
2019-01-24 18:11:23 +0800

    加入蜂鸣器

---
+++ Beep/Source/beep.c
@@ -0,0 +1,57 @@
+/*******************************************************************************
+* 文件名：main.c
+* 描  述：
+* 作  者：CLAY
+* 版本号：v1.0.0
+* 日  期: 2019年1月24日
+* 备  注：Beep(x);响x毫秒；Beep(0)；不响 Beep(-1)；一直响
+*         
+*******************************************************************************
+*/
+
+#include "beep.h"
+
+s32 BeepTimer = 0;
+
+void BeepInit(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB|RCC_APB2Periph_AFIO, ENABLE);
+	GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE); 
+	//GPIO_PinRemapConfig(GPIO_Remap_SWJ_NoJTRST,ENABLE);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;//推挽模式输出
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	
+	BeepOff();//初始化关闭蜂鸣器
+}
+
+void BeepScan(s32 ms)
+{
+	if(BeepTimer > 0)
+	{
+		BeepTimer -= ms;
+		if(BeepTimer <= 0)
+		{
+			BeepOff();
+			BeepTimer = 0;
+		}
+	}
+}
+
+void Beep(s32 time)
+{
+	BeepTimer = time;
+	
+	if(BeepTimer == 0)
+	{
+		BeepOff();
+	}
+	else
+	{
+		BeepOn();
+	}
+}
+
---
+++ Beep/Source/main.c
@@ -0,0 +1,51 @@
+/*******************************************************************************
+* 文件名：main.c
+* 描  述：
+* 作  者：CLAY
+* 版本号：v1.0.0
+* 日  期: 2019年1月24日
+* 备  注：按键B1切换LED1状态并且蜂鸣器鸣叫0.1s
+*         按键B2按下发生长鸣;按键B3停止鸣叫
+*******************************************************************************
+*/
+
+#include "stm32f10x.h"
+#include "led.h"
+#include "key.h"
+#include "timer.h"
+#include "beep.h"
+
+
+int main(void)
+{
+	LEDInit();
+	KeyInit();
+	BeepInit();
+	TIM2Init(2000, 72);//定时2ms
+	
+	while(1)
+	{	
+		KeyDriver();
+	}
+}
+
+void KeyAction(int code)
+{
+	if(code == 1)//按下B1，切换灯状态，蜂鸣器鸣叫0.1s
+	{
+		GPIOC->ODR ^= (1<<8);//PC8不断取反
+		GPIOD->ODR |= (1<<2);//PD2置1，使能573锁存器
+		GPIOD->ODR &= ~(1<<2);//PD2清0，关闭573锁存器
+		Beep(100);
+	}
+	else if(code == 2)
+	{
+		Beep(-1);
+	}
+	else if(code == 3)
+	{
+		Beep(0);
+	}
+}
+
+
