3dea6301a64f16c34c75cb2c6c1d6034ff971622
CLAY <317400877@qq.com>
2019-02-18 17:58:44 +0800

    PWM模式

---
+++ PWM模式_again/Sre/main.c
@@ -0,0 +1,52 @@
+/*******************************************************************************
+* 文件名：main.c
+* 描  述：
+* 作  者：CLAY
+* 版本号：v1.0.0
+* 日  期: 2019年2月18日
+* 备  注：修改后的LCD例程
+*         PA1 (TIM2_CH2) 输出频率500Hz，占空比60%的方波
+*******************************************************************************
+*/
+
+#include "stm32f10x.h"
+#include "lcd.h"
+#include "e2prom.h"
+#include "stdio.h"
+#include "i2c.h"
+#include "adc.h"
+#include "rtc.h"
+#include "usart2.h"
+#include "pwm.h"
+
+u32 TimingDelay = 0;
+u8 RxdCnt = 0;
+u8 RxdOver = 0;
+u8 RxdBuf[20];
+void Delay_Ms(u32 nTime);
+u8 RTC_Flag = 0;
+
+//Main Body
+int main(void)
+{	
+	STM3210B_LCD_Init();
+	LCD_Clear(Blue);
+	LCD_SetBackColor(Blue);
+	LCD_SetTextColor(White);
+	
+	SysTick_Config(SystemCoreClock/1000);
+	
+	PWM_Init(500, 60);//500Hz 60%方波
+	
+	while(1)
+	{
+		
+	}
+}
+
+//
+void Delay_Ms(u32 nTime)
+{
+	TimingDelay = nTime;
+	while(TimingDelay != 0);	
+}
