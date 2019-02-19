2ef7ec7d37c000021e29c32f7c7ac9fbf8d31405
CLAY <317400877@qq.com>
2019-02-19 09:45:38 +0800

    LED_Again

---
+++ KEY_again/Inc/pwm.h
@@ -0,0 +1,7 @@
+#ifndef _PWM_H
+#define _PWM_H
+
+
+void PWM_Init(u32 freq, u8 duty);
+
+#endif
---
+++ KEY_again/Sre/led.c
@@ -0,0 +1,24 @@
+#include "stm32f10x.h"
+
+void LED_Init(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC|RCC_APB2Periph_GPIOD, ENABLE);//设能PC和PD口
+	
+	//锁存器IO配置
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;//强推挽输出
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+	
+	//8个LED的IO配置
+	GPIO_InitStructure.GPIO_Pin = 0xFF00;//PC8~PC15对应的LED位置
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;//强推挽输出
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+	
+	GPIOD->ODR |= (1<<2);//使能573
+	GPIOC->ODR |= 0xFF00;//初始状态熄灭小灯
+	GPIOD->ODR &= (1<<2);//失能573
+}
